8fd073b881f40b357a9dc7c79833afc65495a1bf
Yahya Bulat <138808138+Beatknife@users.noreply.github.com>
2024-05-22 11:40:53 +0300

    Added random number selector

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// first pc should choose rps
+// then human should choose rps
+// the return values should be evaluated
+// logic determines who won and displays a message
+
+function getRandomNumber () {
+    return Math.floor(Math.random() * 10);
+}
+

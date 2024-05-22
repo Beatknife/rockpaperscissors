afd48652ea615800ed74f07c0df48e6c789ea155
Yahya Bulat <138808138+Beatknife@users.noreply.github.com>
2024-05-22 12:07:01 +0300

    Added computer choice function

--- javascript.js
+++ javascript.js
@@ -4,6 +4,28 @@
 // logic determines who won and displays a message
 
 function getRandomNumber () {
-    return Math.floor(Math.random() * 10);
+    return Math.floor(Math.random() * 3 + 1);
+}
+
+function getComputerChoice () {
+
+    switch(getRandomNumber()) {
+        
+        case 1:
+            console.log("rock");
+            break
+
+        case 2:
+            console.log("paper");
+            break
+
+        case 3:
+            console.log("scissors");
+            break
+    }
+}
+
+function getHumanChoice () {
+    prompt("Rock? Paper? Scissors?")
 }
 
